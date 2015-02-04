0d69b99b445b03ba156429322ec889af2675ee2c
Justina Lee <j.tinalee@gmail.com>
2015-02-04 23:55:00 +0800

    Added Constant class.

---
+++ ToDooo/src/application/Constant.java
@@ -0,0 +1,14 @@
+package application;
+public final class Constant {
+	public static final String CATEGORY_UNCATEGORISED = "Uncategorised";
+	
+	public static final String TAG_FILE = "file";
+	public static final String TAG_NEXT_ID = "nextid";
+	public static final String TAG_CATEGORIES = "categories";
+	public static final String TAG_CATEGORY = "category";
+	public static final String TAG_TASKS = "tasks";
+	public static final String TAG_TASK = "task";
+	
+	public static final Command[] CRUD_COMMANDS = { Command.ADD, Command.DELETE, 
+													Command.UPDATE, Command.SEARCH };
+}
