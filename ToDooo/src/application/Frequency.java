47154aa2a9e9627a49dd7c5a2ba9230220309088
Justina Lee <j.tinalee@gmail.com>
2015-02-09 00:33:09 +0800

    Added boolean check methods to Frequency and Priority class.

--- ToDooo/src/application/Frequency.java
+++ ToDooo/src/application/Frequency.java
@@ -2,5 +2,21 @@ package application;
 public enum Frequency {
 	NIL,
 	WEEKLY,
-	MONTH
+	MONTHLY,
+	YEARLY;
+	
+	public static boolean isWeekly(String commandLine) {
+		return (commandLine.contains(Command.RECURRING_WEEKLY.getBasicCommand()) ||
+				commandLine.contains(Command.RECURRING_WEEKLY.getAdvancedCommand()));
+	}
+	
+	public static boolean isMonthly(String commandLine) {
+		return (commandLine.contains(Command.RECURRING_MONTHLY.getBasicCommand()) ||
+				commandLine.contains(Command.RECURRING_MONTHLY.getAdvancedCommand()));
+	}
+	
+	public static boolean isYearly(String commandLine) {
+		return (commandLine.contains(Command.RECURRING_YEARLY.getBasicCommand()) ||
+				commandLine.contains(Command.RECURRING_YEARLY.getAdvancedCommand()));
+	}
 }
--- ToDooo/src/application/Priority.java
+++ ToDooo/src/application/Priority.java
@@ -3,5 +3,20 @@ public enum Priority {
 	NEUTRAL,
 	LOW,
 	MEDIUM,
-	HIGH
+	HIGH;
+	
+	public static boolean isLow(String commandLine) {
+		return (commandLine.contains(Command.PRIORITY_LOW.getBasicCommand()) ||
+				commandLine.contains(Command.PRIORITY_LOW.getAdvancedCommand()));
+	}
+	
+	public static boolean isMedium(String commandLine) {
+		return (commandLine.contains(Command.PRIORITY_MEDIUM.getBasicCommand()) ||
+				commandLine.contains(Command.PRIORITY_MEDIUM.getAdvancedCommand()));
+	}
+	
+	public static boolean isHigh(String commandLine) {
+		return (commandLine.contains(Command.PRIORITY_HIGH.getBasicCommand()) ||
+				commandLine.contains(Command.PRIORITY_HIGH.getAdvancedCommand()));
+	}
 }
