46911af074dfb617ffa179958de53e330933cf45
Justina Lee <j.tinalee@gmail.com>
2015-02-09 00:34:20 +0800

    Filled in Task constructor (incomplete). Need to complete genereateToDoString, getDatesFromString method and to set _id.

--- ToDooo/src/application/Task.java
+++ ToDooo/src/application/Task.java
@@ -19,7 +19,23 @@ public class Task {
 	private Priority _priority;
 	
 	public Task(String userInput, TaskType taskType) {
-		// incomplete
+		// -id = list.readNextId() (use the list object to call)
+		// to be done after writing the method to read setting file
+		// append D, E, F or T to id 
+		
+		_taskType = taskType;
+		_toDo = generateToDoString(userInput, taskType); // incomplete
+		_originalText = userInput;
+		
+		Date[] dates = getDatesFromString(userInput, taskType); // incomplete
+		setDatesForTaskType(dates, taskType);
+		
+		_category = getCategoryFromString(userInput);
+		
+		_isRecurring = Command.isRecurred(userInput);
+		_repeat = getFrequencyFromString(userInput);
+		
+		_priority = getPriorityFromString(userInput);
 	}
 	
 	private String generateToDoString(String userInput, TaskType taskType) {
@@ -34,7 +50,7 @@ public class Task {
 				// remove the slash in /from & /to
 				break;
 			case DATED :
-				// remove the slash in /on
+				// remove the slash in /by
 				break;
 			default :
 				// refactoring advice is to always have default
@@ -43,5 +59,95 @@ public class Task {
 		}
 		
 		return ""; // return the processed string.
+	}
+	
+	private Date[] getDatesFromString(String userInput, TaskType taskType) {
+		Date[] dates = new Date[2];
+		
+		switch(taskType) {
+			case EVENT :
+				// get the parameter after /on
+				// time is after comma<whitespace> behind date
+				break;
+			case TIMED : 
+				// get the parameter after /from
+				// time is after comma<whitespace> behind date
+				
+				// get the parameter after /to
+				// time is after comma<whitespace> behind date
+				break;
+			case DATED :
+				// get the parameter after /by
+				// time is after comma<whitespace> behind date
+				break;
+			default :
+				// floating task
+				// does not require action since no date in array
+				break;
+		}
+		
+		return dates;
+	}
+	
+	private String getCategoryFromString(String userInput) {
+		boolean isCategorised = userInput.contains(Command.CATEGORY.getBasicCommand()) &&
+								!Command.isPrioritised(userInput);
+		
+		if (isCategorised) {
+			int commandIndex = userInput.indexOf(Command.CATEGORY.getBasicCommand());
+			String concatString = userInput.substring(commandIndex + 1, userInput.length());
+			
+			return concatString.substring(0, concatString.indexOf(" "));
+		}
+		
+		return Constant.CATEGORY_UNCATEGORISED;
+	}
+	
+	private void setDatesForTaskType(Date[] dates, TaskType taskType) {
+		_date = null;
+		_from = null;
+		_to = null;
+		_by = null;
+		
+		switch(taskType) {
+			case EVENT :
+				_date = dates[0];
+				break;
+			case TIMED : 
+				_from = dates[0];
+				_to = dates[1];
+				break;
+			case DATED :
+				_by = dates[0];
+				break;
+			default :
+				// floating task
+				// does not require action since no date in array
+				break;
+		}
+	}
+
+	private Frequency getFrequencyFromString(String userInput) {
+		if (Frequency.isWeekly(userInput)) {
+			return Frequency.WEEKLY;
+		} else if (Frequency.isMonthly(userInput)) {
+			return Frequency.MONTHLY;
+		} else if (Frequency.isYearly(userInput)) {
+			return Frequency.YEARLY;
+		} else {
+			return Frequency.NIL;
+		}
+	}
+	
+	private Priority getPriorityFromString(String userInput) {
+		if (Priority.isHigh(userInput)) {
+			return Priority.HIGH;
+		} else if (Priority.isMedium(userInput)) {
+			return Priority.MEDIUM;
+		} else if (Priority.isHigh(userInput)) {
+			return Priority.HIGH;
+		} else {
+			return Priority.NEUTRAL;
+		}
 	}
 }
