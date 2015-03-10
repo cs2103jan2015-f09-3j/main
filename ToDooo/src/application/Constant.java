66532930a02e0c10e706eb70a34ce73c6eef91ce
Phway Phway Ngwe <PHWAY PHWAY NGWE@a0112537.stu.nus.edu.sg>
2015-03-10 23:39:31 +0800

    Added date formats in Constant.java. Minor changes in GUI.

--- ToDooo/src/application/Constant.java
+++ ToDooo/src/application/Constant.java
@@ -111,8 +111,10 @@ public final class Constant {
 	public static final String COLOUR_SUCCESS = "#9ACC77";
 	
 	public static final SimpleDateFormat DATEOUTPUT = new SimpleDateFormat("dd MMMMMMMMM yyyy, EEEEEEEEE");
+	public static final SimpleDateFormat TIMEOUTPUT = new SimpleDateFormat("hh:mm a");
+	public static final SimpleDateFormat DATEOUTPUT_FOR_TIMEDTASK = new SimpleDateFormat("dd MMM yyyy");
 	
 	public static final KeyCombination SHORTCUT_UNDO = new KeyCodeCombination(KeyCode.Z, KeyCombination.SHIFT_DOWN);
 	public static final KeyCombination SHORTCUT_REDO = new KeyCodeCombination(KeyCode.Y, KeyCombination.SHIFT_DOWN);
-	public static final KeyCombination SHORTCUT_OPENFILEDIALOG = new KeyCodeCombination(KeyCode.F10);
+	public static final KeyCombination SHORTCUT_OPEN_FILE_DIALOG = new KeyCodeCombination(KeyCode.F10);
 }
